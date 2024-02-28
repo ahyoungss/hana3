d5f9f2683034961bbcf2838177bb3886dec3ed43
ahyoungss <sallyajoa.com@gmail.com>
2024-02-28 16:35:48 +0900

    deploy

---
+++ react/rbvite/src/main.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import Sample from './components/Sample';
+import { CounterProvider } from './contexts/counter-context.tsx';
+import { HashRouter } from 'react-router-dom';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <CounterProvider>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </CounterProvider>
+    <Sample />
+  </React.StrictMode>
+);
